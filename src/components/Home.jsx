88e1056a2645f089c9429070bf7576e1fdb75427
Andrew Smarty <smartyleey@gmail.com>
2021-05-15 04:20:12 +0100

    ADD: Initial commit for tesla

---
+++ src/components/Home.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import styled from 'styled-components';
+import Section from './Section';
+
+function Home(props) {
+    return (
+        <div>
+            <Section 
+                title="Model S"
+                description="Order Online for Touchless Deliver"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+                backgroundImg="model-s.jpg"
+            />
+            <Section 
+                title="Model Y"
+                description="Order Online for Touchless Deliver"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+                backgroundImg="model-y.jpg"
+            />
+            <Section 
+                title="Model 3"
+                description="Order Online for Touchless Deliver"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+                backgroundImg="model-3.jpg"
+            />
+            <Section 
+                title="Model X"
+                description="Order Online for Touchless Deliver"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+                backgroundImg="model-x.jpg"
+            />
+            <Section 
+                title="Lowest Cost Solar Panel"
+                description="Money Back Guarantee"
+                leftBtnText="Custom Order"
+                rightBtnText="Learn"
+                backgroundImg="solar-panel.jpg"
+            />
+            <Section 
+                title="Solor For New Roofs"
+                description="Solar Roof Costs Less Than a New Roof Plus Solar Panels"
+                leftBtnText="Custom Order"
+                rightBtnText="Learn"
+                backgroundImg="solar-roof.jpg"
+            />
+            <Section 
+                title="Accessories"
+                description=""
+                leftBtnText="Custom Order"
+                    backgroundImg="accessories.jpg"
+            />
+        </div>
+    );
+}
+
+export default Home;
+
+const container = styled.div`
+    height: 100vh;
+`
